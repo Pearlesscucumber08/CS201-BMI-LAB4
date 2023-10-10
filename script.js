455dd6ec9bdd1206cb318511dc2ae65b3490f975
Brigitte Mae Ibeng <115964613+Pearlesscucumber08@users.noreply.github.com>
2023-10-10 14:56:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = " Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB4");
+
+let fruit = "banana";
+switch (fruit){
+    case "banana":
+        console.log("Banana is good!");
+         break;
+    case "orange":
+        console.log("I am not a fan of orange");
+        break;
+    case "Apple":
+        console.log("How you like them apples?");
+        break;
+    default:
+        console.log("I have never heard of that fruit.");
+        break;
+}
